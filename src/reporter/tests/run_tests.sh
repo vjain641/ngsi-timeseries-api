098cef14fc309b7c90ea29c59f819de7d9070ee2
Vipin Jain <vipin.jain@india.nec.com>
2019-06-25 14:29:07 +0530

    Update run_tests.sh

--- src/reporter/tests/run_tests.sh
+++ src/reporter/tests/run_tests.sh
@@ -5,6 +5,10 @@ docker build -t quantumleap ../../../
 docker-compose up -d
 sleep 12
 
+echo "print the value of env varibales"
+echo $ORION_URL
+echo $QL_URL
+
 docker run -ti --rm --network tests_reportertests quantumleap pytest reporter/tests
 r=$?
 
